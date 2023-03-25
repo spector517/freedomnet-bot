c69be2c561c673316e959a0bb7b89b2ed67601a6
spector517 <borunov.a.a@icloud.com>
2023-03-25 18:05:20 +0300

    small fixes + log build service

--- src/main/java/ru/borun/freedomnet/jenkins/Build.java
+++ src/main/java/ru/borun/freedomnet/jenkins/Build.java
@@ -1,6 +1,7 @@
 package ru.borun.freedomnet.jenkins;
 
 import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
 import ru.borun.freedomnet.bot.data.ClientData;
 import ru.borun.freedomnet.http.HttpSender;
 import ru.borun.freedomnet.http.InvalidHttpStatusCode;
@@ -14,10 +15,14 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+@Log4j2
 @Getter
 public class Build {
 
     private static final String AUTH;
+    private static final int UPDATE_BUILD_SUCCESS_HTTP_CODE = 200;
+    private static final int RUN_BUILD_HTTP_SUCCESS_CODE = 204;
+    private static final int GET_JOB_HTTP_SUCCESS_CODE = 200;
 
     static {
         AUTH = "Basic " + Base64.getEncoder().encodeToString(
@@ -38,53 +43,73 @@ public class Build {
 
     public Build(ClientData clientData, JenkinsConfig.Job job, Map<String, String> params)
             throws IOException, InterruptedException, InvalidHttpStatusCode {
+        log.info("Run Jenkins job {} for client id {}", job.getUri(), clientData.getClientId());
         this.clientData = clientData;
         this.job = job;
         this.params = params;
         jenkinsConfig = JenkinsConfig.getInstance();
-        setJobData();
+        getJobInfo();
         runBuild();
         var attemptsCount = Math.floorDiv(
                 jenkinsConfig.getWaitingBuildMaxTimeout(),
                 jenkinsConfig.getPollingInterval()
         );
-        for (int i = 0; i <= attemptsCount; i++) {
+        for (int i = 1; i <= attemptsCount; i++) {
             try {
+                log.debug("Try update build, attempt {}", i);
                 update();
+                break;
             } catch (InvalidHttpStatusCode ex) {
                 if (i == attemptsCount) {
+                    log.error("Max retries ({}) exceeded.", attemptsCount);
+                    log.error(ex);
                     throw ex;
                 }
+                log.debug("Update failed.");
+                log.debug("Sleeping for {} sec...", jenkinsConfig.getPollingInterval() / 1000);
                 Thread.sleep(jenkinsConfig.getPollingInterval());
             }
         }
+        log.info("Build started successfully: {}.", buildData.getUrl());
     }
-    private void setJobData() throws IOException, InterruptedException, InvalidHttpStatusCode {
+
+    private void getJobInfo() throws IOException, InterruptedException, InvalidHttpStatusCode {
+        log.debug("Get job data from Jenkins...");
         var url = String.format("%s/%s/api/json", jenkinsConfig.getUrl(), job.getUri());
         jobData = HttpSender.newHttpSender()
                 .url(url)
                 .auth(AUTH)
                 .build()
-                .sendRequest(JobData.class);
+                .sendRequest(JobData.class, List.of(GET_JOB_HTTP_SUCCESS_CODE));
+        log.debug("Job data received.");
     }
 
     private void runBuild() throws IOException, InterruptedException, InvalidHttpStatusCode {
         var pars = new LinkedHashMap<>(params);
         pars.put("token", job.getToken());
+        log.debug(
+                """
+                        Run job {} with parameters:
+                        {}""",
+                jobData.getUrl(), pars
+        );
         HttpSender.newHttpSender()
                 .url(String.format("%sbuildWithParameters", jobData.getUrl()))
                 .auth(AUTH)
                 .queryParams(pars)
                 .build()
-                .sendRequest();
+                .sendRequest(List.of(RUN_BUILD_HTTP_SUCCESS_CODE));
+        log.debug("Build started.");
     }
 
     public void update() throws IOException, InterruptedException, InvalidHttpStatusCode {
+        log.debug("Updating build...");
         var url = String.format("%s%d/api/json", jobData.getUrl(), jobData.getNextBuildNumber());
         buildData = HttpSender.newHttpSender()
                 .url(url)
                 .auth(AUTH)
                 .build()
-                .sendRequest(BuildData.class, List.of(200));
+                .sendRequest(BuildData.class, List.of(UPDATE_BUILD_SUCCESS_HTTP_CODE));
+        log.debug("Build updated.");
     }
 }
--- src/main/java/ru/borun/freedomnet/jenkins/BuildService.java
+++ src/main/java/ru/borun/freedomnet/jenkins/BuildService.java
@@ -1,11 +1,13 @@
 package ru.borun.freedomnet.jenkins;
 
 import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
 
 import java.util.Objects;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+@Log4j2
 public class BuildService implements Runnable {
 
     private static final BuildService buildService = getInstance();
@@ -24,6 +26,7 @@ public class BuildService implements Runnable {
     }
 
     public void addBuildToProcessing(Build build) {
+        log.info("Add build for client ({}) to processing", build.getClientData().getClientId());
         processingBuilds.add(build);
     }
 
@@ -32,6 +35,7 @@ public class BuildService implements Runnable {
     }
 
     public void stop() {
+        log.info("Stopping build service...");
         stopped = true;
     }
 
@@ -39,6 +43,7 @@ public class BuildService implements Runnable {
     @Override
     public void run() {
         while (!stopped) {
+            log.debug("Sleeping for {} sec", jenkinsConfig.getPollingInterval() / 1000);
             Thread.sleep(jenkinsConfig.getPollingInterval());
             var build = processingBuilds.poll();
             if (build == null) {
@@ -49,7 +54,9 @@ public class BuildService implements Runnable {
                 processingBuilds.add(build);
             } else {
                 finishedBuilds.add(build);
+                log.info("Build for client ({}) processed.", build.getClientData().getClientId());
             }
         }
+        log.info("Build service stopped.");
     }
 }
