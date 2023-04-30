0f5863a063d3352a83f4567348bd0b7e98939ed3
spector517 <borunov.a.a@icloud.com>
2023-04-30 13:40:34 +0300

    small refactor configs tests

--- src/test/java/ru/borun/freedomnet/bot/BotConfigTest.java
+++ src/test/java/ru/borun/freedomnet/bot/BotConfigTest.java
@@ -9,26 +9,12 @@ import ru.borun.freedomnet.common.Config;
 import java.util.Optional;
 
 @DisplayName("Test Bot config load")
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class BotConfigTest {
 
     private static final String CONFIG_PATH = "config.yaml";
     private static final String CONFIG_PREFIX = "bot";
 
     @Test
-    @Order(1)
-    @DisplayName("Invalid config.yaml (no 'bot' section)")
-    @SneakyThrows
-    void testInvalidConfig() {
-        try (var configMock = Mockito.mockStatic(Config.class)) {
-            configMock.when(() -> Config.readConfigMap(CONFIG_PATH, CONFIG_PREFIX))
-                    .thenReturn(Optional.empty());
-            Assertions.assertNull(BotConfig.getInstance());
-        }
-    }
-
-    @Test
-    @Order(2)
     @DisplayName("Successfully load Bot config")
     @SneakyThrows
     void testGetInstanceSuccess() {
--- src/test/java/ru/borun/freedomnet/jenkins/JenkinsConfigTest.java
+++ src/test/java/ru/borun/freedomnet/jenkins/JenkinsConfigTest.java
@@ -10,26 +10,12 @@ import ru.borun.freedomnet.common.Config;
 import java.util.Optional;
 
 @DisplayName("Test Bot config load")
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class JenkinsConfigTest {
 
     private static final String CONFIG_PATH = "config.yaml";
     private static final String CONFIG_PREFIX = "jenkins";
 
     @Test
-    @Order(1)
-    @DisplayName("Invalid config.yaml (no 'jenkins' section)")
-    @SneakyThrows
-    void testInvalidConfig() {
-        try (var configMock = Mockito.mockStatic(Config.class)) {
-            configMock.when(() -> Config.readConfigMap(CONFIG_PATH, CONFIG_PREFIX))
-                    .thenReturn(Optional.empty());
-            Assertions.assertNull(JenkinsConfig.getInstance());
-        }
-    }
-
-    @Test
-    @Order(2)
     @DisplayName("Successfully load Jenkins config")
     @SneakyThrows
     void testGetInstanceSuccess() {
